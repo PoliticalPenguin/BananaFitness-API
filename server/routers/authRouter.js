88d4aad70b3b3ea61afec3f50f91209396fd1ac3
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-06 17:54:48 -0700

    Test user-specific callback URI

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -42,13 +42,14 @@ router.route('/signout')
   });
 
 //Fitbit Oauth Routes
-router.route("/fitbit/authorize")
+router.route("/fitbit/authorize/:username")
   .get(function (req, res) {
     client.getRequestToken().then(function (results) {
       var token = results[0],
         secret = results[1];
       requestTokenSecrets[token] = secret;
-      res.redirect("http://www.fitbit.com/oauth/authorize?oauth_token=" + token);
+      res.redirect("http://www.fitbit.com/oauth/authorize?oauth_token=" + token 
+        + "&redirect_uri=https%3A%2F%2Fpenguin-banana-fitness-api.herokuapp.com%2Fauth%2Ffitbit%2Fcallback%2F" + req.params.username);
     }, function (error) {
       res.send(error);
     });
@@ -59,6 +60,7 @@ router.route("/fitbit/callback")
     var token = req.query.oauth_token,
       secret = requestTokenSecrets[token],
       verifier = req.query.oauth_verifier;
+
     client.getAccessToken(token, secret, verifier).then(function (results) {
       var accessToken = results[0],
         accessTokenSecret = results[1],
