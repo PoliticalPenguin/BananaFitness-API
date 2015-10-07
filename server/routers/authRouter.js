167b8427764269e3656f79b1d15c42e227efcd81
Nate Willard <nate.a.willard@gmail.com>
2015-10-07 10:05:27 -0700

    Add profile to fitbit/request

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -82,16 +82,20 @@ router.route("/fitbit/callback")
 
 router.route("/fitbit/request")
   .get(function (req, res) {
-    return client.get("/activities/heart/date/2015-10-06.json", req.cookies.fitbitAuth.fitBitToken, 
+      return client.get("/profile.json", accessToken, accessTokenSecret).then(function (results) {
+              var response = results[0];              
+              res.cookie('fitbitAuth', credentials, { maxAge: 900000 });
+              res.send(response);
+            });
+      }, function (error) {
+          res.send(error);
+    });
+    /*return client.get("/activities/heart/date/2015-10-06.json", req.cookies.fitbitAuth.fitBitToken, 
       req.cookies.fitbitAuth.fitBitSecret).then(function (results) {
         res.send(results);
       }); 
     }, function (error) {
-      res.send(error);
+      res.send(error);*/
 });
-
-
-
-
 
 module.exports = router;
