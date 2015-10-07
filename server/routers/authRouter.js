4c8b6ae70a4dbdaaeb68b13990138adc093d326f
Nate Willard <nate.a.willard@gmail.com>
2015-10-07 10:52:30 -0700

    Further debugging

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -82,13 +82,13 @@ router.route("/fitbit/callback")
 
 router.route('/fitbit/request/')
   .get(function(req, res) {
-    res.send(req.cookies.fitbitAuth);
-    /*client.get('/profile.json', req.cookies.fitbitAuth.fitbitToken, req.cookies.fitbitAuth.fitbitSecret).then(function(results) {
-      res.send(results);
+    //res.send(req.cookies.fitbitAuth);
+    client.get('/profile.json', req.cookies.fitbitAuth.fitbitToken, req.cookies.fitbitAuth.fitbitSecret).then(function(results) {
+      res.send('results');
     })
     .catch(function(err) {
-      res.send(err);
-    });*/
+      res.send('err');
+    });
   });
 
 /*router.route("/fitbit/request")
