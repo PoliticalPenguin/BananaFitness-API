4c50ecb38689f5286af32e1a2e3276d420000b46
Nate Willard <nate.a.willard@gmail.com>
2015-10-07 11:16:37 -0700

    attempt with hardcoded tokens

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -85,7 +85,7 @@ router.route("/fitbit/callback")
 router.route('/fitbit/request/')
   .get(function(req, res) {
     //res.send(req.cookies.fitbitAuth.fitbitSecret);
-    client.get('/profile.json', req.cookies.fitbitAuth.fitbitToken, req.cookies.fitbitAuth.fitbitSecret).then(function(results) {
+    client.get('/profile.json',  accessToken, accessTokenSecret).then(function(results) {
       res.send(results);
     })
     .catch(function(err) {
