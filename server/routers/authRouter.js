53bdaec2628edc1dc378274e62cd0e9204b9a9b4
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-06 19:55:43 -0700

    Fix potential Fitbit API request syntax error

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -82,7 +82,13 @@ router.route("/fitbit/callback")
 
 router.route("/fitbit/request")
   .get(function (req, res) {
-    res.send(req.cookies);
+    return client.get("/activities/heart/date/2015-10-06.json", req.cookies.fitbitAuth.fitBitToken, 
+      req.cookies.fitbitAuth.fitBitSecret).then(function (results) {
+        res.send(results);
+      }); 
+    }, function (error) {
+      res.send(error);
+    });
 });
 
 
