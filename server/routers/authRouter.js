24793e86994f35eb01f60c437b29a6f3da8b9b04
Nate Willard <nate.a.willard@gmail.com>
2015-10-07 10:25:30 -0700

    Test alternate response sending

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -82,14 +82,15 @@ router.route("/fitbit/callback")
 
 router.route("/fitbit/request")
   .get(function (req, res) {
-      return client.get("/profile.json", accessToken, accessTokenSecret).then(function (results) {
+    res.send("Test");
+      /*return client.get("/profile.json", accessToken, accessTokenSecret).then(function (results) {
               var response = results[0];              
               res.cookie('fitbitAuth', credentials, { maxAge: 900000 });
               res.send(response);
             });
       }, function (error) {
-          res.send(error);
-    });
+          res.send(error);*/
+  });
     /*return client.get("/activities/heart/date/2015-10-06.json", req.cookies.fitbitAuth.fitBitToken, 
       req.cookies.fitbitAuth.fitBitSecret).then(function (results) {
         res.send(results);
