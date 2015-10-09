e59911c56cc4ff47d8bbf86e305f0952c69f1121
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-09 12:13:16 -0700

    Edit Access-Control-Allow headers to fix CORS bug

--- server/server.js
+++ server/server.js
@@ -80,10 +80,10 @@ app.use(express.static(__dirname + '/client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Credentials', false);
   if (req.method === 'OPTIONS') {
     res.status(200);
     res.end();
