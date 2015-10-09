eb330a715c687a4877c6d62504a2d09c48d5120b
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-09 12:15:31 -0700

    Edit false to 'false'

--- server/server.js
+++ server/server.js
@@ -83,7 +83,7 @@ app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', false);
+  res.header('Access-Control-Allow-Credentials', 'false');
   if (req.method === 'OPTIONS') {
     res.status(200);
     res.end();
