9a8315b272b7e209524a9a06c64b03d1c149f4b0
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-09 12:27:31 -0700

    Revert Access-Control-Allow header changes

--- server/server.js
+++ server/server.js
@@ -80,15 +80,10 @@ app.use(express.static(__dirname + '/client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(function(req, res, next) {
-  res.set({
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
-    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept',
-    'Access-Control-Allow-Credentials': false});
-  // res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
-  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
-  // res.header('Access-Control-Allow-Credentials', true);
+  res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
+  res.header('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     res.status(200);
     res.end();
