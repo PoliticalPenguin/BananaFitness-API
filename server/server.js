64b4103db46748a37fa66325a45d69884aab82f5
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-09 11:55:29 -0700

    Remove credential requirement

--- server/server.js
+++ server/server.js
@@ -80,10 +80,8 @@ app.use(express.static(__dirname + '/client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     res.status(200);
     res.end();
