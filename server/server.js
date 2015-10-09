f22c7256bff104ac195f804559f8a78fc237fea3
Christian Ling <christian.s.h.ling@gmail.com>
2015-10-09 12:25:14 -0700

    Aggregate Access-Control-Allow headers in an object

--- server/server.js
+++ server/server.js
@@ -80,10 +80,15 @@ app.use(express.static(__dirname + '/client'));
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
-  res.header('Access-Control-Allow-Credentials', 'false');
+  res.set({
+    'Access-Control-Allow-Origin': 'http://localhost:8100',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,OPTIONS',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept',
+    'Access-Control-Allow-Credentials': true});
+  // res.header('Access-Control-Allow-Origin', 'http://localhost:8100');
+  // res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+  // res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, X-AUTHENTICATION, X-IP, Content-Type, Accept');
+  // res.header('Access-Control-Allow-Credentials', true);
   if (req.method === 'OPTIONS') {
     res.status(200);
     res.end();
